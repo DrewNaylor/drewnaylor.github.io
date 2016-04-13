108f464409a01845d81d8ad825c1ada05038697c
Drew Naylor <DrewNaylor@users.noreply.github.com>
2016-04-13 11:21:54 -0400

    Update JSClock.

--- js/JSClock.js
+++ js/JSClock.js
@@ -1,6 +1,6 @@
-// Clock Part 1 - Put Anywhere Before Part 2
+// <!-- Clock Part 1 - Put Anywhere Before Part 2 -->
 
-
+<script language="JavaScript">
 // Anytime Anywhere Web Page Clock Generator
 // Clock Script Generated at
 // http://www.rainbow.arch.scriptmania.com/tools/clock
@@ -13,11 +13,13 @@ function dT(){ if(fr==0){ fr=1; document.write('<font size=2 face=Arial><b><span
 function aP(x){ return (x>11)?'pm':'am'; } 
 function y4(x){ return (x<500)?x+1900:x; } 
 var dN=new Array('Sunday','Monday','Today is: Tuesday','Wednesday','Thursday','Friday','Saturday'),mN=new Array('January','February','March','April','May','June','July','August','September','October','November','December'),fr=0,oT="dN[tS().getDay()]+' '+tS().getDate()+dE(tS().getDate())+' '+mN[tS().getMonth()]+' '+y4(tS().getYear())+' '+':'+':'+' '+tH(tS().getHours())+':'+lZ(tS().getMinutes())+':'+lZ(tS().getSeconds())+' '+aP(tS().getHours())";
+</script>
 
-
-// Clock Part 1 - Ends Here
-// Clock Part 2 - This Starts/Displays Your Clock
+// <!-- Clock Part 1 - Ends Here  -->
+// <!-- Clock Part 2 - This Starts/Displays Your Clock -->
 
 <script language="JavaScript">dT();</script>
 
-// Clock Part 2 - Ends Here
+// <!-- Clock Part 2 - Ends Here -->
+
+
